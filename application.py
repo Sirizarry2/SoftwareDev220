aeda2975ca441cd77a4673b0bde4053acb810242
Sirizarry2 <152753168+Sirizarry2@users.noreply.github.com>
2024-02-09 13:01:00 -0500

    Add files via upload

---
+++ application.py
@@ -0,0 +1,59 @@
+import requests
+import json
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(80), unique=True)
+    author = db.Column(db.String(80))
+    publisher = db.Column(db.String(80))
+    
+    def __repr__(self) -> str:
+        return f"id {self.id}, book_name {self.book_name}, " + \
+            f"author {self.author} publisher {self.publisher}"
+
+@app.route('/books')
+def get_books():
+    books = Book.query.all()
+
+    output = []
+    for book in books:
+        book_data = {
+            'id': book.id,
+            'book_name': book.book_name,
+            'author': book.author,
+            'publisher': book.publisher
+        }
+        output.append(book_data)
+
+    return {'books': output}
+
+@app.route('/books/<id>')
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return {"name": book.book_name}
+
+@app.route('/books', methods=['POST'])
+def create_book() -> str:
+    book = Book()
+    book.id = request.json['id']
+    book.book_name = request.json['book_name']
+    book.author = request.json['author']
+    book.publisher = request.json['publisher']
+    db.session.add(book)
+    db.session.commit()
+    return {'id': book.id}
+
+@app.route('/books/<id>', methods = ['DELETE'])
+def delete_books():
+    book = Book.query.get(id)
+    if book is None:
+        return {"error": "not found"}
+    db.session.delete(book)
+    db.session.commit()
+    return {"message": "yeet!@"}
